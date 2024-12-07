4f6255fcfd796d0417c246db3e5129931f8d19da
FixCrap <george_andriopoulos@hotmail.gr>
2024-12-07 21:37:37 +0200

    Implementing Tailwind configuring it and the public folder

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,7 @@
-import React from "react";
-
 const App = () => {
 	return (
 		<main>
-			<h1 className='text-5xl text-orange-500 font-bold'>
+			<h1 className='text-5xl text-violet-300 font-circular-web text-orange-500 font-bold'>
 				Welcome to Awwwards
 			</h1>
 		</main>
